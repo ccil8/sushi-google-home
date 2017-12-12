72e2e7503259007ed68ec670c59b34ed557bc39f
palimpZest <polo.vaca365@gmail.com>
2017-12-12 18:22:12 +0100

    feat: Add first test DBs and routes

--- sushi-home/routes/index.js
+++ sushi-home/routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const products = require("../product-database.js");
+const stores = require("../store-database.js");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/products", function(req, res, next) {
+  res.render("products"), { products };
+  console.log(products);
+});
+
+router.get("/stores", function(req, res, next) {
+  res.render("stores"), { stores };
+  console.log(stores);
+  console.log("##########################################");
+  console.log("##########################################");
+  console.log(stores[0].id_store);
+  console.log(stores[0].name);
 });
 
 module.exports = router;
